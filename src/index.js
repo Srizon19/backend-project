651deae6c3285ea3c731bdfe988d60d7efc42cf8
Rizwan Ahmed <50909722+Srizon19@users.noreply.github.com>
2024-08-27 07:45:44 +0600

    connected database

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
- 
+import dotenv from "dotenv";
+
+import mongoose from "mongoose";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
+
+
+//  import express from "express";
+// const app = express();
+
+
+//ifie to connect DB
+
+// (async () => {
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+//         app.on("error", (err)=>{
+//             console.log(`ERRR: `, err)
+//         })
+
+//         app.listen(process.env.port, () => {
+//             console.log(`app listening on the port ${process.env.PORT}`)
+//         })
+//     }
+//     catch (error){
+//         console.error("ERROR: ", error)
+//     }
+// })()
